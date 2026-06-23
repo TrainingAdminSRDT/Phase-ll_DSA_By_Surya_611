33705e6b568993c635fc8712d5e29e87bcc97865
amanawasthi-lang <amanawasthi9170723061@gmail.com>
2026-06-23 12:10:12 +0530

    Add files via upload

---
+++ sum1.java
@@ -0,0 +1,22 @@
+public class sum1 {
+    public static void main(String[] args) {
+      int  x = 2;
+      int   y = --x + x++ + x-- ;
+        System.out.print(x + " "+y);
+//
+//        int i= -1 , j=-1, k=-1 , l=2 , m ;
+//        m = ((i++ && j++ && k++)||(l++));
+//        System.out.println(i,j,k,l,m);
+    }
+
+
+
+}
+
+// y= 1+3 +2
+//why java and c++ give different answer
+// i = 2 what is i here
+  // for each  - collection of elements
+// for( : )
+
+
